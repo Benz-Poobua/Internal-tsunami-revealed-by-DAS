65ce31f3762d7cda27f2e4f5353d65c65735fbdb
Benz Poobua <supakorn1207@gmail.com>
2024-11-02 01:39:08 -0700

    Remove unused functions

--- util/Data_processing.py
+++ util/Data_processing.py
@@ -24,7 +24,7 @@ def data_concat(file_list):
     # function to read and process each CSV file
     def read_and_process(file_path):
         df = pd.read_csv(file_path, header=None)
-        df.columns = ['datetime', 'phase (rad)']
+        df.columns = ['datetime', 'optical distance along cable (m)']
         df = df.drop_duplicates(subset=['datetime'])
         df['datetime'] = pd.to_datetime(df['datetime'])
         df = df.sort_values(by='datetime').reset_index(drop=True)
@@ -49,58 +49,8 @@ def data_concat(file_list):
     return concatenated_df
 
 def prep_data(concatenated_df, window='60s'):
-    resample_df = concatenated_df.resample('60s').mean()
+    resample_df = concatenated_df.resample(window).mean()
     interpolate_df = resample_df.interpolate()
 
     return interpolate_df
-
-def compute_fft(phases_df):
-    """
-    Compute the FFT of the phase values in the given DataFrame.
-    
-    Parameters:
-    phases_df (DataFrame): DataFrame containing 'datetime' and 'phase (rad)' columns.
-    
-    Returns:
-    positive_frequencies (ndarray): Array of positive frequency values.
-    positive_magnitude (ndarray): Array of corresponding magnitudes.
-    """
-    # extract phase values and time index
-    phase_values = phases_df['phase (rad)'].values
-    time_index = phases_df.index
-    
-    # calculate time difference (seconds)
-    dt = (time_index[1] - time_index[0]).total_seconds()  # time difference in seconds; uniformly sampled
-    n = len(phase_values)  # number of samples
-    
-    # perform the FFT
-    fft_values = np.fft.fft(phase_values)
-    fft_magnitude = np.abs(fft_values) / n  # normalize the amplitude
-    #fft_magnitude /= np.max(fft_magnitude) # optional
-    
-    # get frequency bins
-    fft_frequency = np.fft.fftfreq(n, d=dt)  # frequency bins
-    
-    # only keep the positive values
-    positive_frequencies = fft_frequency[:n // 2]
-    positive_magnitude = fft_magnitude[:n // 2]
-    
-    return positive_frequencies, positive_magnitude
-
-def peak_iden(positive_frequencies):
-    
-    f_min = 1/(60*60)
-    f_max = 1/(5*50)
-
-    freq_mask = (positive_frequencies >= f_min) & (positive_frequencies <= f_max)
-
-    peaks, _ = find_peaks(freq_mask, threshold=np.log10(3))
-
-    top_peaks = peaks[:3] 
-
-    peak_values = positive_frequencies[top_peaks]
-
-    print("Indices of local maxima:", top_peaks)
-    print("Values of local maxima:", peak_values)
-
-    return top_peaks, peak_values
+ 
